259f701a35e329065bb606ef7f9357925f2091c7
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-17 11:34:12 +0300

    define some methods

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -7,9 +7,20 @@ namespace topit {
   template< class T >
   struct Vector {
     Vector();
+    Vector(const Vector &);
+    Vector(Vector &&);
     ~Vector();
+    Vector & operator=(const Vector &);
+    Vector & operator=(Vector &&);
 
     bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+    size_t getCapacity() const noexcept;
+
+    void pushBack(const T & val);
+    void popBack();
+    void insert(size_t i, const T & val);
+    void erase(size_t i);
 
   private:
     T * data_;
@@ -19,19 +30,25 @@ namespace topit {
 
 template< class T >
 topit::Vector< T >::Vector():
-  data_(),
-  size_(),
-  capacity_()
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
 {}
 
 template< class T >
 topit::Vector< T >::~Vector()
-{}
+{
+  delete [] data_;
+}
 
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
   return size_ == 0;
 }
+
+template< class T >
+void topit::Vector< T >::pushBack(const T & val)
+{}
 
 #endif
