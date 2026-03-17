a55f447f93f783b6984815c096d592746f354699
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-17 11:12:03 +0300

    add test framework

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <iomanip>
 #include "top-it-vector.hpp"
 
+bool testEmptyVector()
+{
+  return false;
+}
+
 int main()
-{}
+{
+  using test_t = std::pair< const char *, bool(*)() >;
+  test_t tests[] = {
+    {"Empty vector", testEmptyVector}
+  };
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  std::cout << std::boolalpha;
+  bool pass = true;
+  for (size_t i = 0; i < count; ++i) {
+    bool res = tests[i].second();
+    std::cout << tests[i].first << ": " << res << "\n";\
+    pass = pass && res;
+  }
+  std::cout << "RESULT: " << pass << "\n";
+}
