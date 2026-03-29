226bba8770bea7553cc9d83f9a14fc38e4eafd4f
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-29 13:21:09 +0300

    add arithmetic operators

--- viter.hpp
+++ viter.hpp
@@ -20,7 +20,7 @@ namespace topit {
     VIter< T > operator--(int);
     VIter< T > operator+(std::ptrdiff_t n) const;
     VIter< T > operator-(std::ptrdiff_t n) const;
-    std::ptrdiff_t operator-(const VIter< T > & other);
+    std::ptrdiff_t operator-(const VIter< T > & other) const;
     bool operator==(const VIter< T > & other) const noexcept;
     bool operator!=(const VIter< T > & other) const noexcept;
     bool operator<(const VIter< T > & other) const noexcept;
@@ -59,6 +59,54 @@ template< class T >
 T & topit::VIter< T >::operator[](std::ptrdiff_t n) const
 {
   return ptr_[n];
+}
+
+template< class T >
+topit::VIter< T > & topit::VIter< T >::operator++()
+{
+  ++ptr_;
+  return *this;
+}
+
+template< class T >
+topit::VIter< T > topit::VIter< T >::operator++(int)
+{
+  VIter old = *this;
+  ++(*this);
+  return old;
+}
+
+template< class T >
+topit::VIter< T > & topit::VIter< T >::operator--()
+{
+  --ptr_;
+  return *this;
+}
+
+template< class T >
+topit::VIter< T > topit::VIter< T >::operator--(int)
+{
+  VIter old = *this;
+  ++(*this);
+  return old;
+}
+
+template< class T >
+topit::VIter< T > topit::VIter< T >::operator+(std::ptrdiff_t n) const
+{
+  return VIter< T >{ptr_ + n};
+}
+
+template< class T >
+topit::VIter< T > topit::VIter< T >::operator-(std::ptrdiff_t n) const
+{
+  return VIter< T >{ptr_ - n};
+}
+
+template< class T >
+std::ptrdiff_t topit::VIter< T >::operator-(const VIter< T > & other) const
+{
+  return ptr_ - other.ptr_;
 }
 
 #endif
